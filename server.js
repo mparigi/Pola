0ec95c556233554a51470f0a7dac751fb0c37591
Makarand <mparigi99@gmail.com>
2016-08-04 19:42:33 -0400

    Created SItemap Skeleton
    Created html files and server redirections. No content yet.

--- server.js
+++ server.js
@@ -6,8 +6,45 @@ var port = 3000;
 
 app.use(express.static(__dirname + "/public"));
 
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/login", function(req, res) {
+    res.sendFile(__dirname + "/login.html");
+});
+
+app.get("/signup", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.get("/polls/:pollID", function(req, res) {
+    res.end("Poll with pollID " + req.params.pollID);
+});
+
+app.get("/create", function(req, res) {
+    if (/*authenticated user*/true) {
+        res.sendFile(__dirname + "/create.html");
+    } else {
+        res.sendFile(__dirname + "/no-acccount-error.html");
+    }
+});
+
+app.get("/mypolls", function(req, res) {
+    if (/*authenticated user*/true) {
+        res.sendFile(__dirname + "/mypolls.html");
+    } else {
+        res.sendFile(__dirname + "/no-acccount-error.html");
+    }
+});
+
+app.get("/settings", function(req, res) {
+    if (/*authenticated user*/true) {
+        res.sendFile(__dirname + "/settings.html");
+    } else {
+        res.sendFile(__dirname + "/no-acccount-error.html");
+    }
 });
 
 app.listen(port);
