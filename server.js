403d97617cef3c810352b88c4856e6d2479b641c
Makarand <mparigi99@gmail.com>
2016-08-04 17:10:02 -0400

    Set up basic express server
    simply shows hello world from html file.

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(port);
