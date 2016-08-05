d7abf0864dddcfe7c1528b0223406e896abb17c3
Makarand <mparigi99@gmail.com>
2016-08-04 21:11:07 -0400

    Convert to Pug
    Converted html files to pug files. Set View engine rendering in server. https://github.com/pugjs/pug

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var express = require('express');
-var key = require('key.js');
+var key = require('./key.js');
 
 var app = express();
 var port = 3000;
 
-app.use(express.static(__dirname + "/public"));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index.pug");
 });
 
 app.get("/login", function(req, res) {
-    res.sendFile(__dirname + "/login.html");
+    res.render("login.pug");
 });
 
 app.get("/signup", function(req, res) {
-    res.sendFile(__dirname + "/signup.html");
+    res.render("signup.pug");
 });
 
 app.get("/polls/:pollID", function(req, res) {
@@ -25,25 +26,25 @@ app.get("/polls/:pollID", function(req, res) {
 
 app.get("/create", function(req, res) {
     if (/*authenticated user*/true) {
-        res.sendFile(__dirname + "/create.html");
+        res.render("create.pug");;
     } else {
-        res.sendFile(__dirname + "/no-acccount-error.html");
+        res.render("no-acccount-error.pug");
     }
 });
 
 app.get("/mypolls", function(req, res) {
     if (/*authenticated user*/true) {
-        res.sendFile(__dirname + "/mypolls.html");
+        res.render("mypolls.pug");
     } else {
-        res.sendFile(__dirname + "/no-acccount-error.html");
+        res.render("no-acccount-error.pug");
     }
 });
 
 app.get("/settings", function(req, res) {
     if (/*authenticated user*/true) {
-        res.sendFile(__dirname + "/settings.html");
+        res.render("settings.pug");
     } else {
-        res.sendFile(__dirname + "/no-acccount-error.html");
+        res.render("no-acccount-error.pug");
     }
 });
 
